5a4907eb768912d9eb2f559781a04d0475d92091
maababubakr1 <maababubakr1@gmail.com>
2023-08-16 13:30:37 +0000

    one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *description: prints _putchar, followed by a new line.
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(c[i]);
+	return (0);
+}
