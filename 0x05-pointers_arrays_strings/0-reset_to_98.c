8e8b8b82d6defd5b17e64c163912176b2b3780bb
maababubakr1 <maababubakr1@gmail.com>
2023-08-21 16:41:20 +0000

    C _arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer as parameter and updates the value.
+ *@n : an integer variable
+ *Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
