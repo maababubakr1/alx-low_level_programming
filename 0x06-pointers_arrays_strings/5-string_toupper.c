bf6ac48fb17a0ff5dd52f24d905ffa7480dfd179
maababubakr1 <maababubakr1@gmail.com>
2023-08-24 22:33:36 +0000

    n

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *string_toupper - A function that changes all lowercase letters to uppercase.
+ *@s: A string to modify.
+ *Return: the changed string
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *cap_string - A function that capitalizes all words of a string.
+ *@s: A string to modify
+ *Return: The resulting string
+ */
+
+char *cap_string(char *)
+{
+	int 
