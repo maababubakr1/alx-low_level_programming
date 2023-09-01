464447e85e51b3e9cc76d00b0c3a4abdb52edaad
maababubakr1 <maababubakr1@gmail.com>
2023-09-01 23:51:20 +0000

    all

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - prints number of arguments
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
