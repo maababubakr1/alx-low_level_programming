88a369bcc65a8f3330b335bd74b580b0c4ec78c7
maababubakr1 <maababubakr1@gmail.com>
2023-09-01 20:17:44 +0000

    f

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -6,41 +6,31 @@
  *
  * Return: the int converted from the string
  */
+
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	while (*s)
 	{
-		if (s[i] == '-')
-			++d;
+		if (*s == '-')
+			sign *= -1;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (*s >= '0' && *s <= '9')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			null_flag = 1;
+			total = total * 10 + *s - '0';
 		}
-		i++;
+
+		else if (null_flag)
+			break;
+		s++;
 	}
 
-	if (f == 0)
-		return (0);
+	if (sign < 0)
+		total = (-total);
 
-	return (n);
+	return (total);
 }
