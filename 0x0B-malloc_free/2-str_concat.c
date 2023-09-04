8bfbc629a6af7f8bc8115099a03a07fa41a0a90c
maababubakr1 <maababubakr1@gmail.com>
2023-09-04 18:44:17 +0000

    all

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,20 +12,21 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int len1, len2, i, j;
 
-	i = 0;
-	j = 0;
 	len1 = 0;
 	len2 = 0;
 
-	while (s1[len1])
+	while (s1 && s1[len1])
 		len1++;
-	while (s2[len2])
+	while (s2 && s2[len2])
 		len2++;
 
 	s3 = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (s3 == NULL)
 		return (NULL);
+
+	i = 0;
+	j = 0;
 
 	if (s1)
 	{
@@ -38,16 +39,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s2)
 	{
-		while (j < len2)
+		while (i < (len1 + len2))
 		{
 			s3[i] = s2[j];
 			i++;
 			j++;
 		}
 	}
-
 	s3[i] = '\0';
-
 	return (s3);
 }
-
