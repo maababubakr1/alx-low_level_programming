1bb8f17585aa38c28934c91251bb339bf03a7c17
maababubakr1 <maababubakr1@gmail.com>
2023-09-05 20:23:28 +0000

    nb

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *strtow - function that splits a string into words
+ *@str: A string to be splitted
+ *Return: pointer to array of strings (Success), NULL (Error)
+ */
+
+char **strtow(char *str)
+{
+	
