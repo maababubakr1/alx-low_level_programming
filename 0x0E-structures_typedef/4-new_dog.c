8b2285bc2776595fcc1a4d34a3c95ed26491c67e
maababubakr1 <maababubakr1@gmail.com>
2023-09-11 16:42:02 +0000

    str

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,4 +10,38 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+	dog_t *new_dog;
+	char *cname, *cowner;
+	int lname = 0, lowner = 0, i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	while (name[lname])
+		lname++;
+	while (owner[lowner])
+		lowner++;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	cname = malloc(lname + 1);
+	if (cname == NULL)
+		return (NULL);
+	for (i = 0; i < name[i]; i++)
+		cname[i] = name[i];
+	cname[i] = '\0';
+
+	cowner = malloc(lowner + 1);
+		if (cowner == NULL)
+			return (NULL);
+	for (i = 0; i < owner[i]; i++)
+		cowner[i] = owner[i];
+	cowner[i] = '\0';
+
+	new_dog->name = cname;
+	new_dog->age = age;
+	new_dog->owner = cowner;
+	return (new_dog);
+}
