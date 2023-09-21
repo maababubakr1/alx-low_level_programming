17cc9e4c30e53c7472d6f958527c1e80d68177d8
maababubakr1 <maababubakr1@gmail.com>
2023-09-21 19:48:11 +0000

    adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void construct(void)__attribute__((constructor));
+
+/**
+ *construct - Prints a sentence before the main function is executed
+ */
+
+void construct(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
