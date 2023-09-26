e91de5f45affd5a67fb95548c8cfeb4dd9c19319
maababubakr1 <maababubakr1@gmail.com>
2023-09-26 21:07:24 +0000

    first

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,29 +8,28 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp = NULL;
-	const listint_t *len = NULL;
+	const listint_t *tmp = NULL;
+	const listint_t *l_n = NULL;
 	size_t count = 0;
-	size_t i;
+	size_t new;
 
-	temp = head;
-	while (temp)
+	tmp = head;
+	while (tmp)
 	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
+		printf("[%p] %d\n", (void *)tmp, tmp->n);
 		count++;
-		temp = temp->next;
-
-		len = head;
-		i = 0;
-		while (i < count)
+		tmp = tmp->next;
+		l_n = head;
+		new = 0;
+		while (new < count)
 		{
-			if (temp == len)
+			if (tmp == l_n)
 			{
-				printf(" -> [%p] %d\n", (void *)temp, temp->n);
+				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
 				return (count);
 			}
-			len = len->next;
-			i++;
+			l_n = l_n->next;
+			new++;
 		}
 		if (!head)
 			exit(98);
